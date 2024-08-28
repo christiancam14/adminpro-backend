96cb3f168a9afa8cf4eafc6c1ff50babd8f075d3
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-08-27 23:51:53 -0500

    Ajustes JWT y middlewares

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const app = express();
 // Configurar CORS
 app.use(cors());
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Base de datos
 dbConnection();
 
 const port = process.env.PORT;
 
 // Rutas
-app.get("/", (req, res) => {
-  res.status(400).json({
-    ok: true,
-    msg: "Hola mundo",
-  });
-});
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/login", require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
